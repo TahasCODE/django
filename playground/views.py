f461dac8c835baa63cb0389294191ce14971a0a1
Taha Khan <tahak5815@gmail.com>
2026-04-28 17:02:41 +0500

    lookups filtering and Q

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from store.models import Product
 
 
-def calculate():
-    x = 1 
-    y = 2
-    return x
+
 
 def say_hello(request):
-    x = calculate()
-    return render(request,'hello.html',{'name':'Taha Waheed'})
+   
+    query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # query_set = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20)) means inventory less than 10 and unit price greater than or equal to 20
+    # returns None if not found
+    # exists = Product.objects.filter(pk=0).exists() # returns True or False
+   
+
+    
+
+    return render(request,'hello.html',{'name':'Taha Waheed','products':list(query_set)})
