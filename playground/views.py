0b0d92d20aba57103743c1237d814e0e4c2fe565
Taha Khan <tahak5815@gmail.com>
2026-04-28 17:34:26 +0500

    Sorting using earliest latest order_by

--- playground/views.py
+++ playground/views.py
@@ -10,7 +10,15 @@ from store.models import Product
 def say_hello(request):
    
     # query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
-    query_set = Product.objects.filter(inventory=F('collection__id'))
+    # query_set = Product.objects.filter(inventory=F('collection__id'))
+    # query_set = Product.objects.order_by('unit_price','title')
+    query_set = Product.objects.order_by('unit_price','title').reverse()
+    # product = Product.objects.order_by('unit_price')[0] # returns the first product with the lowest unit price
+    # product = Product.objects.earliest('unit_price') # returns the first product with the lowest unit price
+    # product = Product.objects.latest('unit_price') # returns the first product with the highest
+
+    
+    # query_set = Product.objects.order_by('-title')
     # query_set = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20)) means inventory less than 10 and unit price greater than or equal to 20
     # returns None if not found
     # exists = Product.objects.filter(pk=0).exists() # returns True or False
