c3d1d32bd7b8bb75723b1aac1aa80c076d63365e
Taha Khan <tahak5815@gmail.com>
2026-04-28 17:38:39 +0500

    Limiting Results using index Slicing

--- playground/views.py
+++ playground/views.py
@@ -12,7 +12,9 @@ def say_hello(request):
     # query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     # query_set = Product.objects.filter(inventory=F('collection__id'))
     # query_set = Product.objects.order_by('unit_price','title')
-    query_set = Product.objects.order_by('unit_price','title').reverse()
+    # query_set = Product.objects.order_by('unit_price','title').reverse()
+    # query_set = Product.objects.all()[:5] # returns the first 5 products
+    query_set = Product.objects.all()[5:10] # returns products from index 5 to index 9
     # product = Product.objects.order_by('unit_price')[0] # returns the first product with the lowest unit price
     # product = Product.objects.earliest('unit_price') # returns the first product with the lowest unit price
     # product = Product.objects.latest('unit_price') # returns the first product with the highest
